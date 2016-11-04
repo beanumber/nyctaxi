a08d328ff04c4befbf389322d871432cc900583c
Trang <trang.lt157@gmail.com>
2016-11-04 10:38:41 -0400

    edit extract for both fare, trip and month

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -13,15 +13,23 @@
 #'    etl_transform() %>% 
 #'    etl_load() 
 
-etl_extract.etl_nyctaxi <- function(obj, ...) {
+etl_extract.etl_nyctaxi <- function(obj, month = 1, trip=TRUE,fare=FALSE,...) {
   message("Extracting raw data...")
   raw_dir <- paste0(attr(obj, "dir"), "/raw")
   if (!dir.exists(raw_dir)) {
     dir.create(raw_dir)
   }
-  
-  local <- paste0(raw_dir, "/trip_data_1.csv.zip")
-  remote <- "https://nyctaxitrips.blob.core.windows.net/data/trip_data_1.csv.zip"
-  etl::smart_download(remote, local)
-  invisible(obj)
+  month <- as.character(month)
+  if(trip) {
+    local <- paste0(raw_dir, "/trip/trip_data_",month,".csv.zip")
+    remote <- paste0("http://nyctaxitrips.blob.core.windows.net/data/trip_data_",month,".csv.zip")
+    etl::smart_download(remote, local)
+    invisible(obj)
+  }
+  if(fare) {
+    local <- paste0(raw_dir, "/fare/trip_fare_",month,".csv.zip")
+    remote <- paste0("http://nyctaxitrips.blob.core.windows.net/data/trip_fare_",month,".csv.zip")
+    etl::smart_download(remote, local)
+    invisible(obj)
+  }
 }
