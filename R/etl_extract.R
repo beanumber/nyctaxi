b1697c5ab5edfdce02b268d765d2ff40e515ca9f
Trang <trang.lt157@gmail.com>
2016-11-09 14:15:28 -0500

    fix URL to get data from nyc.gov in extract() and load()

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -13,17 +13,16 @@
 #'    etl_transform() %>% 
 #'    etl_load() 
 
-etl_extract.etl_nyctaxi <- function(obj, month = 1, color = "yellow",fare=FALSE,...) {
+etl_extract.etl_nyctaxi <- function(obj, month = 1, type  = "yellow", fare=FALSE,...) {
   message("Extracting raw data...")
-  #message("Choose color yellow or green for trip data...")
   raw_dir <- paste0(attr(obj, "dir"), "/raw")
   if (!dir.exists(raw_dir)) {
     dir.create(raw_dir)
   }
   month <- as.character(month)
-  local <- paste0(raw_dir, "/trip/",color,"_tripdata_",year,"-0",month,".csv")
+  local <- paste0(raw_dir, "/",color,"/",type,"_tripdata_",year,"-0",month,".csv")
   remote <- paste0("https://s3.amazonaws.com/nyc-tlc/trip+data/",color,"_tripdata_",year,"-0",month,".csv")
-  smart_download(remote, local,method="curl")
+  download.file(remote, local,method="libcurl")
   #if(fare) {
   # local <- paste0(raw_dir, "/fare/trip_fare_",month,".csv.zip")
   #  remote <- paste0("http://nyctaxitrips.blob.core.windows.net/data/trip_fare_",month,".csv.zip")
--- R/etl_transform.R
+++ R/etl_transform.R
@@ -19,15 +19,15 @@ etl_transform.etl_nyctaxi <- function(obj, month = 1, ...) {
 #' @importFrom DBI dbWriteTable
 #' @import etl
 #' 
-etl_load.etl_nyctaxi <- function(obj, schema = FALSE, month=1,...) {
+etl_load.etl_nyctaxi <- function(obj, schema = FALSE, type="yellow",year="2016", month=1,...) {
   #raw_dir <- paste0(attr(obj, "dir"), "/raw")
   
   #extract only the data of the month we want
-  load_dir <- attr(obj, "load_dir")
-  filename <- paste0("trip_data_", month, ".csv")
-  path <- file.path(load_dir,filename)
+  #load_dir <- attr(obj, "load_dir")
+  #filename <- paste0("trip_data_", month, ".csv")
+  #path <- file.path(load_dir,filename)
   #trip <- read.csv(path)
-  
+  path <- paste0(raw_dir, "/",color,"/",type,"_tripdata_",year,"-0",month,".csv")
   #db <- etl:::verify_con(obj) --> encountered error as this is an S3 object
   #db <- dbConnect(RSQLite::RSQLite(),":memory:") alternative 
 
