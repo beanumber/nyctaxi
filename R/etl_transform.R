89e0252b2444cfc96796b7a5e26916f385be015a
Ben Baumer <ben.baumer@gmail.com>
2016-12-09 11:16:33 -0500

    updated transform and load()

--- R/etl_transform.R
+++ R/etl_transform.R
@@ -1,7 +1,7 @@
 #' @rdname etl_extract.etl_nyctaxi
 #' @export  
 
-etl_transform.etl_nyctaxi <- function(obj, month = 1, type = "yellow", ...) {
+etl_transform.etl_nyctaxi <- function(obj, year = 2016, month = 1, type = "yellow", ...) {
   message("Transforming raw data...")
   
   #directory for src and dest file
@@ -12,6 +12,7 @@ etl_transform.etl_nyctaxi <- function(obj, month = 1, type = "yellow", ...) {
   local <- paste0(raw_dir, "/", type, "_tripdata_", year, "-0", month, ".csv")
   file.copy(from = local, to = load_dir)
   #unzip(path, exdir = load_dir)
+  invisible(obj)
 }
 
 #' @export
@@ -19,15 +20,16 @@ etl_transform.etl_nyctaxi <- function(obj, month = 1, type = "yellow", ...) {
 #' @importFrom DBI dbWriteTable
 #' @import etl
 #' 
-etl_load.etl_nyctaxi <- function(obj, schema = FALSE, type = "yellow", year = "2016", month = 1, ...) {
+etl_load.etl_nyctaxi <- function(obj, year = "2016", month = 1, type = "yellow", ...) {
+  message("Loading data into database...")
   #raw_dir <- paste0(attr(obj, "dir"), "/raw")
   
   #extract only the data of the month we want
-  #load_dir <- attr(obj, "load_dir")
+  load_dir <- attr(obj, "load_dir")
   #filename <- paste0("trip_data_", month, ".csv")
   #path <- file.path(load_dir,filename)
   #trip <- read.csv(path)
-  path <- paste0(raw_dir, "/", type, "_tripdata_", year, "-0", month, ".csv")
+  path <- paste0(load_dir, "/", type, "_tripdata_", year, "-0", month, ".csv")
   #db <- etl:::verify_con(obj) --> encountered error as this is an S3 object
   #db <- dbConnect(RSQLite::RSQLite(),":memory:") alternative 
 
@@ -35,5 +37,5 @@ etl_load.etl_nyctaxi <- function(obj, schema = FALSE, type = "yellow", year = "2
   #drv <- dbDriver("SQLite")
   #db <- dbConnect(drv, dbPath)
   DBI:::dbWriteTable(conn = obj$con, "trips", path)
-  invisible(db)
+  invisible(obj)
 }
