6005c3d92eb1d0c00e9cab8d33acd1b1e7e43b44
liwencong1995 <liwencong1995@gmail.com>
2017-09-21 13:41:34 -0400

    Updated descriptions of function. Added functionalityies to etl_load.

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -1,13 +1,14 @@
-#' Extract data from NYC Taxi Trips
+#' Extract NYC Taxi Trip Data from data from NYC Taxi & Limousine Commission
 #' 
 #' @import etl
 #' @importFrom stringr str_pad
 #' @export 
-#' @details extract data from NYC taxi trips 
-#' @param year a numeric vector giving the years
-#' @param month a numeric vector giving the months
-#' @param types a character vector of \code{yellow} and/or \code{green}. The default 
-#' is \code{yellow}.
+#' @details extract NYC Yellow taxi trip data from Jan 2009 and 
+#' Green taxi trip data from Aug 2013 data from NYC Taxi & Limousine Commission 
+#' @param years a numeric vector giving the years. The default is the most recent year.
+#' @param months a numeric vector giving the months. The default is January to December.
+#' @param types a character vector giving the type of taxi trip data of \code{yellow} 
+#' and/or \code{green}. The default is \code{yellow}.
 #' @inheritParams etl::etl_extract
 #' @seealso \code{\link[etl]{etl_extract}}
 #' @examples 
@@ -15,9 +16,9 @@
 #' taxi <- etl("nyctaxi", dir = "~/Desktop/nyctaxi/")
 #' \dontrun{
 #' taxi %>% 
-#'    etl_extract() %>% 
-#'    etl_transform() %>% 
-#'    etl_load() 
+#'    etl_extract(years = 2016, months = 1:2, types = "yellow") %>% 
+#'    etl_transform(years = 2016, months = 1:2, types = "yellow") %>% 
+#'    etl_load(years = 2016, months = 1:2, types = "yellow") 
 #' }
 
 etl_extract.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),'%Y')), 
@@ -34,13 +35,11 @@ etl_extract.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),'%
   remote <- lapply(types, get_dates, years, months) %>%
     bind_rows() %>%
     mutate(url = paste0("https://s3.amazonaws.com/nyc-tlc/trip+data/", 
-                  type, "_tripdata_", year, "-", stringr::str_pad(month, 2, "left", "0"), ".csv"))
+                  type, "_tripdata_", year, "-", 
+                  stringr::str_pad(month, 2, "left", "0"), ".csv"))
   
   etl::smart_download(obj, remote$url)
 
   invisible(obj)
 }
-
-#do we want to change type into types? 
-#to download both yellow and green cabs data by one commend?
 
--- R/etl_load.R
+++ R/etl_load.R
@@ -3,10 +3,42 @@
 #' @importFrom DBI dbWriteTable
 #' @import etl
 #' 
-etl_load.etl_nyctaxi <- function(obj, years = "2016", months = 1, types = "yellow", ...) {
+etl_load.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),'%Y')), 
+                                 months = 1:12, 
+                                 types  = "yellow", ...) {
+  
+  #get a list of all possible combinations of years, months, and types variables
+  get_dates <- function(x, years, months) {
+    valid_year_month(years, months) %>%
+      mutate(type = x)
+  }
+  
+  #create a list of file names
+  remote <- lapply(types, get_dates, years, months) %>%
+    bind_rows() %>%
+    mutate(file = paste0(type, "_tripdata_", year, "-", 
+                        stringr::str_pad(month, 2, "left", "0"), ".csv")) %>%
+    mutate(src = paste0(attr(obj, "load_dir"),file))
+  
+  #create a dataset for each color
+  file_by_color <- function(data,type) {
+    df <- data %>%
+      filter(file == paste0(type, "_tripdata_", year, "-", 
+                            stringr::str_pad(month, 2, "left", "0"), ".csv"))
+    assign(paste(type,"tripdata",sep = "-"), df )
+    invisible(df)
+  }
+  
+  #check whether the file has already been loaded or not
   match_files_by_year_months(list.files(attr(obj, "load_dir")),
-                             pattern = "yellow_tripdata_%Y_%m.+\\.csv", years = years, months = months)
-  src <- list.files(attr(obj, "load_dir"), pattern = "yellow.+\\.csv", full.names = TRUE)
-  smart_upload(obj, src, tablenames = "yellow")
+                             pattern = "yellow_tripdata_%Y_%m.+\\.csv", 
+                             years = years, months = months)
+  
+  #load the files in the color df
+  load_color_file <- function(data,type) {
+    #src <- list.files(attr(obj, "load_dir"), pattern = "yellow.+\\.csv", full.names = TRUE)
+    smart_upload(obj, src, tablenames = paste(type,"tripdata",sep = "-"))
+  }
+    
   invisible(obj)
 }
