f9a5bf5fadb92da4c0fe2db470bcf6bee8b2efa0
Ben Baumer <ben.baumer@gmail.com>
2017-09-21 14:33:55 -0400

    fixed etl_load()

--- R/etl_load.R
+++ R/etl_load.R
@@ -17,28 +17,9 @@ etl_load.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),'%Y')
   remote <- lapply(types, get_dates, years, months) %>%
     bind_rows() %>%
     mutate(file = paste0(type, "_tripdata_", year, "-", 
-                        stringr::str_pad(month, 2, "left", "0"), ".csv")) %>%
-    mutate(src = paste0(attr(obj, "load_dir"),file))
+                        stringr::str_pad(month, 2, "left", "0"), ".csv"), 
+           src = file.path(attr(obj, "load_dir"), file))
   
-  #create a dataset for each color
-  file_by_color <- function(data,type) {
-    df <- data %>%
-      filter(file == paste0(type, "_tripdata_", year, "-", 
-                            stringr::str_pad(month, 2, "left", "0"), ".csv"))
-    assign(paste(type,"tripdata",sep = "-"), df )
-    invisible(df)
-  }
-  
-  #check whether the file has already been loaded or not
-  match_files_by_year_months(list.files(attr(obj, "load_dir")),
-                             pattern = "yellow_tripdata_%Y_%m.+\\.csv", 
-                             years = years, months = months)
-  
-  #load the files in the color df
-  load_color_file <- function(data,type) {
-    #src <- list.files(attr(obj, "load_dir"), pattern = "yellow.+\\.csv", full.names = TRUE)
-    smart_upload(obj, src, tablenames = paste(type,"tripdata",sep = "-"))
-  }
-    
+  smart_upload(obj, src = remote$src, tablenames = remote$type)
   invisible(obj)
 }
