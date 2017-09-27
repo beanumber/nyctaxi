74872e39a9efcb29f190aab9edbeaf80789b0407
liwencong1995 <liwencong1995@gmail.com>
2017-09-26 22:40:58 -0400

    Added test for mysql connection.

--- tests/testthat/test-nyctaxi.R
+++ tests/testthat/test-nyctaxi.R
@@ -10,3 +10,13 @@ test_that("etl nyctaxi", {
   #taxi %>% etl_extract()
   
 })
+
+test_that("etl nyctaxi mysql", {
+  #expect_equal(2 * 2, 4)
+  db <- src_mysql("nyctaxi", user = "WencongLi", host = "localhost", password = "P320718")
+  taxi <- etl("nyctaxi", db)
+  expect_s3_class(taxi,c("etl_nyctaxi","etl","src_mysql","src_sql","src"))
+  #taxi %>% etl_extract()
+})
+
+
