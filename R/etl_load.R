a9e1f4cc7def8660e0ef2228fa5478964cd1c64b
liwencong1995 <liwencong1995@gmail.com>
2017-09-28 11:36:37 -0400

    Modified 'etl_load' so that it will check whether the files requested by uers exist in the load directory before loading them. #8

--- R/etl_load.R
+++ R/etl_load.R
@@ -16,9 +16,21 @@
 etl_load.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),'%Y')), 
                                  months = 1:12, 
                                  types  = "yellow", ...) {
+  #deal with the blank row in green taxi cab data
+  remote <- get_file_path(years, months, types, path = attr(obj, "load_dir")) %>%
+    mutate(skip = ifelse(type == "green", 1, 0))
   
-  remote <- get_file_path(years, months, types, path = attr(obj, "load_dir")) 
-
-  smart_upload(obj, src = remote$src, tablenames = remote$type)
+  #create a df of file path of the files that are in the load directory
+  src <- list.files(attr(obj, "load_dir"), "\\.csv", full.names = TRUE)
+  src <- data.frame(src)
+  
+  #only keep the files thst the user wants to transform
+  src_small <- inner_join(remote,src,by = "src")
+  
+  mapply(DBI::dbWriteTable, name = src_small$type, value = src_small$src,
+         MoreArgs = list(conn = obj$con, append = TRUE, ... = ...))
+  
+  #smart_upload(obj, src = remote$src, tablenames = remote$type)
+  
   invisible(obj)
 }
