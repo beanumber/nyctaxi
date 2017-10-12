c53fa7337ebd651e1d8ccba80b20c3a1fa8fff30
liwencong1995 <liwencong1995@gmail.com>
2017-10-12 11:47:25 -0400

    Added code that delete the blank row in each green taxicab dataset.

--- R/etl_transform.R
+++ R/etl_transform.R
@@ -15,6 +15,7 @@ etl_transform.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),
   
   #create a df of file path of the files that the user wants to transform
   remote <- get_file_path(years, months, types, path = attr(obj, "raw_dir")) 
+  remote_green <- remote %>% filter(type == "green")
   
   #create a df of file path of the files that are in the raw directory
   src <- list.files(attr(obj, "raw_dir"), "\\.csv", full.names = TRUE)
@@ -22,18 +23,29 @@ etl_transform.etl_nyctaxi <- function(obj, years = as.numeric(format(Sys.Date(),
   #only keep the files thst the user wants to transform
   src_small <- intersect(src, remote$src)
   
+  src_small_green <- intersect(src, remote_green$src)
+  
   if (length(src_small) == 0) {
     message("The files you requested are not available in the raw directory.")
   } else {
     #find the load directory
     lcl <- file.path(attr(obj, "load_dir"), basename(src_small))
+    lcl_green <- file.path(attr(obj, "raw_dir"), basename(src_small_green))
     
+    # check that the type is green, and then do this
+    if (.Platform$OS.type == "unix"){
+      cmds <- paste("sed -i -e '2d'", lcl_green)
+      lapply(cmds, system)
+    } else {
+      message("Windows system does not currently support this function...")
+    }
+    
+    #copy the files in the raw directory and paste them to the load directory
     file.copy(from = src_small, to = lcl)
   }
   
-  # check that second line is blank, and then do this
-  cmds <- paste("sed -i -e '2d'", lcl)
-  lapply(cmds, system)
+  
+  
   
   #is it caused by invisible obj?
   invisible(obj)
