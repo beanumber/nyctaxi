02b26a0bbf07265676df615da1fb0a8b3a4b7afd
Priscilla Li <priscilla@Priscillas-MacBook-Air.local>
2017-10-30 16:20:04 -0400

    Added nyc taxi_zone_loopup data. #18

--- data-raw/taxi_zone.R
+++ data-raw/taxi_zone.R
@@ -1,1 +1,5 @@
 #get taxi zone info from Github Repository 
+download.file("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/taxi-zone-lookup.csv",
+              "~/Desktop/taxi_zone_lookup.csv")
+taxi_zone_lookup <- read.csv("~/Desktop/taxi_zone_lookup.csv")
+save(taxi_zone_lookup, file = "taxi_zone_lookup.rda")
